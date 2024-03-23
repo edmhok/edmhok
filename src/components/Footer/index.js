3073460ac6e40f845f3f51183120366334123edd
mhok <edzone.ece@gmail.com>
2024-03-23 22:51:04 +0800

    working init

---
+++ src/components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Socials from "../Socials";
+import Link from "next/link";
+import Button from "../Button";
+
+const Footer = ({}) => {
+  const calendlyUrl = 'https://calendly.com/mhokzone/30min'; // Replace with your actual Calendly link
+  
+  return (
+    <>
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+        <div>
+          <h1 className="text-2xl text-bold">Contact.</h1>
+          <div className="mt-10">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              LET&apos;S WORK
+            </h1>
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              TOGETHER
+            </h1>
+            <Button type="primary" onClick={() => window.open(calendlyUrl, '_blank')}>Schedule a call</Button>
+            <div className="mt-10">
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+        Made With ❤ by{" "}
+        <Link href="http://www.mhokzone.com">
+          <a className="underline underline-offset-1">Mhokzone</a>
+        </Link>
+      </h1> */}
+    </>
+  );
+};
+
+export default Footer;
